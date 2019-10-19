c7c3392d159cf835890e4ec547c921e29e8aeec3
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-19 18:21:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def countdown(number)
   while number > 0 
   puts "#{number} SECOND(S)!"
@@ -15,3 +16,14 @@ def countdown_with_sleep(number)
 end
  "HAPPY NEW YEAR!"
 end
+=======
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep(1)
+  puts "HAPPY NEW YEAR!"
+end
+end
+>>>>>>> 533080f54ee98ef7ac408d5412b35ab8e9ea66d2
